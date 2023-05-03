1312fad707adeef80491f68b9744b384e06f5c4b
Ray Zilli <3222523+rayzilli@users.noreply.github.com>
2023-05-03 14:35:39 -0400

    game plays until winner is decided
    game declares a winner on console.

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 
+const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  
+  function checkWinner(board) {
+    return winConditions.some(combination => combination.every(index =>board[index] !== '' && board[index] === board[combination[0]]));
+  }
+
+    
+
 const gameBoard = (function(){
     let board = ['','','','','','','','',''];
     const getBoard = () => {
@@ -16,7 +33,7 @@ const displayBoard = function(){
     const board = gameBoard.getBoard();
     const square = document.querySelectorAll('.square');
     square.forEach((square, index) => {
-        console.log(`Updating square ${index} with value ${board[index]}`); 
+        // console.log(`Updating square ${index} with value ${board[index]}`); 
         square.textContent = board[index];
     });
 };
@@ -29,15 +46,16 @@ const displayController = (function(){
     const board = gameBoard.getBoard();
     for(let i = 0; i<square.length; i++){
         square[i].addEventListener('click', (e) => {
-            console.log(e.target.dataset.square)
-            board.splice((e.target.dataset.square)-1, 1, currentValue)
+            board.splice((e.target.dataset.square)-1, 1, currentValue);
+            displayBoard(board);
+            if (checkWinner(board)){
+                console.log(`${currentValue} wins!`);
+            }
             currentValue = currentValue === 'X' ? 'O' : 'X';
-            displayBoard();
         }, { once: true });
     }
+ 
 })();
 
 
-
-    
 
